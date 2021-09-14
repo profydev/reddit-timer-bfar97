ed751f62420af2c650ebdceb2778841515834ec9
Bernardo Rodrigues <bfar97@gmail.com>
2021-09-14 22:15:05 +0100

    m first folder and project structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import 'normalize.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/search" />
+          <Route path="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
