ed52463add2b5d9d3dcd16e8c0b65e1af2c91049
Bernardo Rodrigues <bfar97@gmail.com>
2021-09-14 22:34:44 +0100

    added navbar component to the main app page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
