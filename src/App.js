d717a0273df67a4a592d8d170bdd055e83b1af7b
Bernardo Rodrigues <bfar97@gmail.com>
2021-09-14 22:24:47 +0100

    ok so this is me trying to figure why didnt work 1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
